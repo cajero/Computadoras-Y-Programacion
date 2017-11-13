5c62819f3ad28a9769c0be868ac08637537637bd
gabriel elias cajero salazar <31681506+cajero@users.noreply.github.com>
2017-11-13 11:55:29 -0600

    TAREA 21 NOMBRE ..USANDO ..PUNTEOS..

---
+++ tAREA 21  mi nombre CON PUNTEROS.cpp
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+void Menu1();
+void letra1();
+void letra2();
+void letra3();
+void letra4();
+void letra5();
+
+int main()
+{
+	letra1(); letra2(); letra3(); letra4(); letra5();
+	system("PAUSE");
+    return 0;
+}
+
+void Menu1(){}
+
+void letra1() {
+	printf("\teeeeee\n");
+	printf("\tee\n");
+	printf("\teeeee\n");
+	printf("\tee\n");
+	printf("\teeeeee\n\n");
+}
+
+void letra2() {
+	printf("\tll\n");
+	printf("\tll\n");
+	printf("\tll\n");
+	printf("\tll\n");
+	printf("\tlllllll\n\n");
+}
+
+void letra3() {
+	printf("\tiiiiii\n");
+	printf("\t  ii\n");
+	printf("\t  ii\n");
+	printf("\t  ii\n");
+	printf("\tiiiiii\n\n");
+}
+
+void letra4() {
+	printf("\t    a\n");
+	printf("\t   a a\n");
+	printf("\t  aaaaa\n");
+	printf("\t a      a\n");
+	printf("\ta        a\n\n");
+}
+
+void letra5() {
+	printf("\tsssssss\n");
+	printf("\tss\n");
+	printf("\tsssssss\n");
+	printf("\t     ss\n");
+	printf("\tsssssss\n\t\t\t");
+}
